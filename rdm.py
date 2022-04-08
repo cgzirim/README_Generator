e4b95d3ef597925066999ea4abd1142414d4fa72
iChigozirim <iamokwukwe@gmail.com>
2022-04-08 12:03:32 +0100

    Update file.

--- rdm.py
+++ rdm.py
@@ -4,6 +4,7 @@ import sys
 from os.path import isdir
 from commands import create, task, author
 from get_args import arguments
+from helper_functions import refresh_json_file
 
 """Documentary"""
 args = arguments(sys.argv)
@@ -18,12 +19,17 @@ match args[1]:
         if create(path) == 0:
             sys.exit()
     case "task":
+        refresh_json_file()
         if task() == 0:
             sys.exit()
     case "author":
+        refresh_json_file()
         if author(args[2]) == 0:
             sys.exit()
     case unknown_command:
-        print("Help message")
+        print("Usage:\n" +
+            "  Create README.md - ./rdm.py create path\n" +
+            "  Add task         - ./rdm.py task\n" +
+            "  Add authors      - ./rdm.py author")
 
 
