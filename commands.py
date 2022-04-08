d907fbae830a28a0db1d0ad945fe015519eacadd
iChigozirim <iamokwukwe@gmail.com>
2022-04-08 12:26:13 +0100

    Update

--- commands.py
+++ commands.py
@@ -1,29 +1,17 @@
-"""
-./rdm create
-check if file exists: readme file already exists. do you want to generate a new one?(n/y)
---> insert project's title:
---> insert URL of this repository
---> projects description
+"""Defines funtions for each command of the README gen."""
 
-"""
-
-"""Defines funtions for each command"""
-import imp
 from os.path import isfile, isdir
-import re
-import json
 from helper_functions import save_to_json_file, load_from_json_file
-from file_sections import get_section
+from helper_functions import insert_content, sort_tasks, count_tasks
 
 
 def create(path):
     """Creates a new README.md file.
     
     Args:
-        path (str): Path to save the README.md file
+        path (str): Path to create the README.md file
 
-    Returns: If the file wasn't created - 0
-             Otherwise - 1.
+    Returns: On success - 1. Otherwise - 0.
     """
     if isfile(path + "README.md"):
         ans = input ("README.md file already exist in this directory. Do you want to generate a new one?(y/n): ")
@@ -37,25 +25,30 @@ def create(path):
     p_title = input ("Project's title: ")
     p_url = input ("Insert URL of this repository: ")
     p_description = input ("Project's description: ")
+    section0_begin = "[comment]: <> (Section_0_begin)\n"
+    section0_end = "\n[comment]: <> (Section_0_end)"
+
+    content = section0_begin + "# " + p_title + "\n" + p_description
+    content += section0_end
 
     with open(path + "README.md", 'w') as rdm_file:
-        rdm_file.write("# " + p_title + "\n" + p_description)
+        rdm_file.write(content)
 
     dictionary = {"path": path, "url": p_url}
-    save_to_json_file(dictionary, ".rdm.txt")
+    save_to_json_file(dictionary, "rdm_gen.json")
     
     return (1)
 
 
 def task():
-    """Add tasks to the README file
+    """Add tasks to the README file below the first section.
     
     Args:
         path (str): Path to save the README.md file.
     Returns: If task was created successfully - 1
              Otherwise - 0
     """
-    dict = load_from_json_file(".rdm.txt")
+    dict = load_from_json_file("rdm_gen.json")
     path = dict["path"]
     p_url = dict["url"]
     
@@ -64,26 +57,56 @@ def task():
         return (0)
 
     with open(path + "README.md", 'r+') as rdm_file:
-        #When task() is called for the first time
-        if '## Tasks' not in rdm_file.read():
-            #Get the project's description section and add 'task' below it.
-            p_d_section = get_section("# ", "\n")
-            rdm_file.seek(p_d_section[1])
-            tsk_title = input ("Task's title: ")
-            rdm_file.write("\n## Tasks \n" + "\t - #### " + tsk_title + "\n")
+        if "[comment]: <> (Section_1_begin)" in rdm_file.read():
+            dict["task_exist"] = 1
+        else:
+            dict["task_exist"] = 0
 
-        #you should probably print format here
-        # and let function promt be num of function.
-        p_d_section = get_section("# ", "## Tasks")
-        rdm_file.seek(p_d_section[1])
+    save_to_json_file(dict, "rdm_gen.json")
 
-        file_info = input ("<file name: file description>: ")
-        file_name = "".join(re.findall("\A.*:", file_info))
-        file_description = "".join(re.findall(":.*", file_info))
-        file_url = p_url + "/" + file_name
-        rdm_file.write("\n\n\t - " + file_url + file_description + "\n")
+    # When task() is called for the first time input the task header and
+    # make it's boundries
+    if dict['task_exist'] == 0:
+        task_header = "## Tasks \n"
+        section1_begin = "\n[comment]: <> (Section_1_begin)\n"
+        section1_end = "\n[comment]: <> (Section_1_end)"
+        dict['tasks_num'] = 1
+    else:
+        task_header = ""
+        section1_begin = ""
+        section1_end = ""
+        dict['tasks_num'] = count_tasks() + 1
 
-        
+    #Task title
+    tsk_title = input ("Task's title: ")
+    
+    #File(s) created to complete task
+    file_info = input ("<file name: file description>: ")
+    file_name = "".join(file_info[:file_info.index(':')])
+    file_description = "".join(file_info[file_info.index(':'):])
+    file_url = p_url + "/" + file_name
+
+    if tsk_title == "" or file_info == "":
+        return (0)
+
+    content =  section1_begin
+    content += task_header
+    content += "[comment]: <> (task_{}_begin)\n".format(dict['tasks_num'])
+    content += "\t - #### " + tsk_title + "\n"
+    content += "\t - " + file_url + file_description + "\n"
+    content += "[comment]: <> (task_{}_end)\n\n".format(dict['tasks_num'])
+    content += section1_end
+
+    if dict['task_exist'] == 0:
+        # Add the task section under section 0
+        insert_content(content, dict['section0'][1] + 1)
+    else:
+        # Add tasks within the task section
+        insert_content(content, dict['section1'][1])
+
+    save_to_json_file(dict, "rdm_gen.json") 
+    sort_tasks()      
+
     return (1)
 
 def author(num_of_authors):
@@ -103,7 +126,7 @@ def author(num_of_authors):
         print("Usage message")
         return 0
 
-    dict = load_from_json_file(".rdm.txt")
+    dict = load_from_json_file("rdm_gen.json")
     path = dict["path"]
 
     if isfile(path + "README.md") is False:
