dbac29bf16204561c2e9b7aed56dfd67efc8b879
root <iamokwukwe@gmail.com>
2022-05-02 00:51:27 -0700

    Use if/else instead of match cases

--- rdm.py
+++ rdm.py
@@ -8,26 +8,26 @@ from helper_functions import refresh_json_file
 
 """Documentary"""
 args = arguments(sys.argv)
+first_arg = args[1]
+if first_arg == "create":
+    #usage: <create path>
+    path = "./"
+    if args[2] is not None:
+        path = args[2]
 
-match args[1]:
-    case "create":
-        #usage: <create path>
-        path = "./"
-        if args[2] is not None:
-            path = args[2]
-
-        if create(path) == 0:
-            sys.exit()
-    case "task":
-        refresh_json_file()
-        if task() == 0:
-            sys.exit()
-    case "author":
-        refresh_json_file()
-        if author(args[2]) == 0:
-            sys.exit()
-    case unknown_command:
-        print("Usage:\n" +
+    if create(path) == 0:
+        sys.exit()
+elif first_arg == "task":
+    refresh_json_file()
+    if task() == 0:
+        sys.exit()
+elif first_arg == "author":
+    refresh_json_file()
+    if author(args[2]) == 0:
+        sys.exit()
+#case unknown_command:
+else:
+    print("Usage:\n" +
             "  Create README.md - ./rdm.py create path\n" +
             "  Add task         - ./rdm.py task\n" +
             "  Add authors      - ./rdm.py author")
